ee608185f99bc47b7a9751b001617b0ef7f06de2
Zubeneshamali <michele.salvi4@studio.unibo.it>
2025-05-19 11:32:41 +0200

    idk

--- main.r
+++ main.r
@@ -9,3 +9,23 @@ test <- tail(data, n = (nrow(data) / 3))
 print(dim(data))
 print(dim(training))
 print(dim(test))
+
+wCPI <- training$PCEPI
+wIPI <- training$INDPRO
+dates <- training$sasdate
+
+train_mean <- training %>%
+  select(!sasdate) %>%
+  mutate_all(mean) %>%
+  t()
+
+train_stdev <- training %>%
+  select(!sasdate) %>%
+  mutate_all(sd) %>%
+  t()
+
+Train_std <- training %>%
+  select(!sasdate) %>%
+  mutate_all( scale, center = T, scale = T) %>%
+  cbind(dates, .)
+
