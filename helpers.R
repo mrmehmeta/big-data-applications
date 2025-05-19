a45ddfa8e5f0534418b647df825a2f0c56ebb984
Mehmet Aydin <64013972+mrmehmeta@users.noreply.github.com>
2025-05-19 15:09:13 +0200

    Helpers

---
+++ helpers.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(zoo)
+
+autoregress <- function(var, p){
+  n <- length(var)
+  y <- var[-c(1:p)]
+  
+  x <- matrix(, nrow = (n-p), ncol = p)
+  
+  for(i in 1:p){
+    x[,i] <- var[(p+1-i):(n-i)]
+  }
+  
+  lm <- lm(y ~ x)
+  
+  return <- list(
+    y = y,
+    x = x,
+    lm = lm
+  )
+}
